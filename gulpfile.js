0e68e33f1d9a3985919c3dcbff23ad7640b5b5d9
Shane Gadsby <schme16@gmail.com>
2015-05-05 17:05:19 +1000

    - First successful gulpfile.js built  - First dist version of snazzy.min.js added  - first test added, but is incomplete
    Signed-off-by: Shane Gadsby <schme16@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -3,41 +3,86 @@ plugins = require('gulp-load-plugins')();
 fs = require('fs');
 del = require('del');
 path = require('path');
+$ = require('cheerio');
+icons = {};
 
 
 
 
 modules = [
-	'modules/base.js',
-	'modules/get.js',
-	'modules/post.js',
+	'src/swiffy/runtime.js',
+	'src/snazzy.js',
+	'tmp/icons.js',
 ]
 
-
-buildOrder = [
-	'Start',
-	'Process files',
-]
 
 gulp.task('Start', function() {
-	del.sync(['dist'], {force: true})
-	return gulp		
+	//del.sync(['dist', 'tmp'], {force: true})
+	return gulp;
 });
 
-gulp.task('build', buildOrder, function() {
-	return gulp		
+gulp.task('Process icons', function() {
+	var files = fs.readdirSync('icons');
+
+	for(var i in files) {
+		var n = files[i].replace('.html', '').replace(/\[|\]/g, ''),
+			page = $(fs.readFileSync('icons/' + files[i]).toString()),
+			scriptTag = page.find('script');
+			
+			//console.log(scriptTag.replace('swiffyobject =', ''));
+			
+			var t = scriptTag[1].children[0].data.replace('swiffyobject =', '').trim();
+			var canvas = page.find('div');
+
+
+
+		icons[n] = {
+			name: n,
+			swiffyobject: JSON.parse(t.substr(0, t.length-1)),
+			width: canvas.css('width').replace('px',''),
+			height: canvas.css('height').replace('px','')
+		}
+	}
+	return plugins.file('icons.js', '(function(){snazzy.icons = ' + JSON.stringify(icons) + '}())', {src:true}).pipe(gulp.dest(process.cwd() + '/tmp'));
 });
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', ['build'], function () {
-	gulp.watch('modules/**/*', ['build']);
-});
-
-gulp.task('Process files', function() {
-	return gulp.src(modules)
+gulp.task('Copy docs', function() {
+	return gulp.src('docs')/*
 	.pipe(plugins.sourcemaps.init())
 	.pipe(plugins.concat('jaxxy.min.js'))
 	.pipe(plugins.uglify())
 	.pipe(plugins.sourcemaps.write('./'))
+	.pipe(gulp.dest('dist'));*/
+});
+
+gulp.task('Concat files', ['Start', 'Process icons', 'Copy docs'], function() {
+	return gulp.src(modules)
+	.pipe(plugins.concat('snazzy.min.js'))
+	//.pipe(plugins.uglify())
 	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('Cleanup', ['Concat files'], function() {
+	del.sync(['tmp'], {force: true})
+	return gulp;
+});
+
+
+
+
+
+
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['build'], function () {
+	gulp.watch(['src/**/*', '/icons/**/*'], ['build']);
+});
+
+
+
+
+// The same as the default, but without the watch
+gulp.task('build', ['Cleanup'], function() {
+	
+	return gulp
 });
--- src/snazzy.js
+++ src/snazzy.js
@@ -1,4 +1,4 @@
 snazzy = (function () {
 	var t = {};
 	return t;
-})
+}());
